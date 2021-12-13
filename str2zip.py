bb26109d7787ec9b472314e5740128fde00820d6
ceaqw <2487689294@qq.com>
2021-12-13 15:01:01 +0800

    add:str2zip

--- str2zip.py
+++ str2zip.py
@@ -1,4 +1,10 @@
+'''
+Date: 2021-11-16 10:16:24
+LastEditTime: 2021-12-13 14:35:00
+Author: ceaqw
+'''
 import os
+import sys
 
 def str2zip(content, zipPath):
     with open(zipPath, 'w', encoding='utf8') as f:
@@ -6,15 +12,24 @@ def str2zip(content, zipPath):
     os.system('zip -o {} {}'.format(zipPath, zipPath))
     os.system('rm {}'.format(zipPath))
 
-index = 0
-for line in a:
-    str2zip(line, index)
-    index += 1
+def getContents(contentPath):
+    with open(contentPath, 'r') as f:
+        return f.read().split("\n")
+
+def convert(contentPath, outPath):
+    contents = getContents(contentPath)
+    index = 0
+    total = len(contents)
+    for line in contents:
+        print("Convert process {}/{}".format(index+1, total))
+        str2zip(line, "{}/{}".format(outPath, index))
+        index += 1
 
 def main():
-    if len(sys.argv) < 4:
-        print("\033[32mTest args format: {} $taskPath $outPath $luaName\033[0m".format(sys.argv[0]))
+    if len(sys.argv) != 3:
+        print("\033[32mTest args format: {} $contentPath $outPath\033[0m".format(sys.argv[0]))
     else:
-        pass
+        convert(sys.argv[1], sys.argv[2])
 
 if __name__ == '__main__':
+    main()
